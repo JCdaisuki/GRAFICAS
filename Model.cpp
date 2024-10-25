05cc6e233f04be371c3b7eb0cff8c5470e4e4bcc
Juan Carlos <99610897+JCdaisuki@users.noreply.github.com>
2024-10-25 19:14:53 +0200

    Model
    Implementado como copia pega del tutorial con leves cambios, requerirá cambios!

--- Model.cpp
+++ Model.cpp
@@ -2,5 +2,117 @@
 
 namespace PAG
 {
+    class ShaderProgram;
 
+    Model::Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices)
+    {
+        this->vertices = vertices;
+        this->indices = indices;
+
+        setupMesh();
+    }
+
+    void Model::Mesh::Draw(ShaderProgram &shader)
+    {
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+    }
+
+    void Model::Mesh::setupMesh()
+    {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
+                     &indices[0], GL_STATIC_DRAW);
+
+        // vertex positions
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+        // vertex normals
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+        // vertex texture coords
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+        glBindVertexArray(0);
+    }
+
+    void Model::Draw(ShaderProgram &shader)
+    {
+        for(unsigned int i = 0; i < meshes.size(); i++)
+            meshes[i].Draw(shader);
+    }
+
+    void Model::loadModel(std::string path)
+    {
+        Assimp::Importer import;
+        const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+        if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+        {
+            throw std::runtime_error("ERROR::ASSIMP::" + std::string(import.GetErrorString()));
+        }
+
+        directory = path.substr(0, path.find_last_of('/'));
+
+        processNode(scene->mRootNode, scene);
+    }
+
+    void Model::processNode(aiNode *node, const aiScene *scene)
+    {
+        // process all the node's meshes (if any)
+        for(unsigned int i = 0; i < node->mNumMeshes; i++)
+        {
+            aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+            meshes.push_back(processMesh(mesh, scene));
+        }
+        // then do the same for each of its children
+        for(unsigned int i = 0; i < node->mNumChildren; i++)
+        {
+            processNode(node->mChildren[i], scene);
+        }
+    }
+
+    Model::Mesh Model::processMesh(aiMesh *mesh, const aiScene *scene)
+    {
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
+
+        for(unsigned int i = 0; i < mesh->mNumVertices; i++)
+        {
+            Vertex vertex;
+
+            glm::vec3 vector;
+            vector.x = mesh->mVertices[i].x;
+            vector.y = mesh->mVertices[i].y;
+            vector.z = mesh->mVertices[i].z;
+            vertex.Position = vector;
+
+            vector.x = mesh->mNormals[i].x;
+            vector.y = mesh->mNormals[i].y;
+            vector.z = mesh->mNormals[i].z;
+            vertex.Normal = vector;
+
+            vertices.push_back(vertex);
+        }
+        
+        for(unsigned int i = 0; i < mesh->mNumFaces; i++)
+        {
+            aiFace face = mesh->mFaces[i];
+            for(unsigned int j = 0; j < face.mNumIndices; j++)
+                indices.push_back(face.mIndices[j]);
+        }
+
+        return Mesh(vertices, indices);
+    }
 }
--- Model.h
+++ Model.h
@@ -1,13 +1,61 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
+#include <iostream>
+#include <vector>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include "imgui/imgui_impl_opengl3_loader.h"
+#include <glad/glad.h>
 
 namespace PAG
 {
+    class ShaderProgram;
+
     class Model
     {
+        struct Vertex {
+            glm::vec3 Position;
+            glm::vec3 Normal;
+            glm::vec2 TexCoords;
+        };
 
+        private:
+
+            class Mesh
+            {
+                public:
+                    // mesh data
+                    std::vector<Vertex>       vertices;
+                    std::vector<unsigned int> indices;
+
+                    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices);
+                    void Draw(ShaderProgram &shader);
+                private:
+                    //  render data
+                    unsigned int VAO, VBO, EBO;
+
+                    void setupMesh();
+            };
+
+            // model data
+            std::vector<Mesh> meshes;
+            std::string directory;
+
+            void loadModel(std::string path);
+            void processNode(aiNode *node, const aiScene *scene);
+            Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+
+        public:
+            Model(char *path)
+            {
+                loadModel(path);
+            }
+            void Draw(ShaderProgram &shader);
     };
 
 }
