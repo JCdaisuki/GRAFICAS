a4e8826f7803dcb643503a32d6381670ea7634ac
Juan Carlos <99610897+JCdaisuki@users.noreply.github.com>
2024-11-08 19:03:11 +0100

    Actualizados shaders

--- Shaders/pag03-fs.glsl
+++ Shaders/pag03-fs.glsl
@@ -2,7 +2,15 @@
 in vec3 colorFragmento;
 out vec4 colorFinal;
 
+in salidaVS
+{
+    vec3 posicionV;
+    vec3 normalV;
+} entrada;
+
 void main()
 {
     colorFinal = vec4(colorFragmento, 1.0);
+
+    vec3 n = normalize ( entrada.normalV );
 }
--- Shaders/pag03-vs.glsl
+++ Shaders/pag03-vs.glsl
@@ -7,9 +7,17 @@ uniform mat4 projection;
 
 out vec3 colorFragmento;
 
+out salidaVS
+{
+    vec3 posicionV;
+    vec3 normalV;
+} salida;
+
 void main()
 {
     gl_Position = projection * view * vec4(posicion, 1.0);
     colorFragmento = colorVertice;
+
+    salida.posicionV = vec3 ( matrizMV * vec4 ( posicion, 1 ) );
 }
 
