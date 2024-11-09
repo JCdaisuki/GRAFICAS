3a4eed4547d21bfce7c9f1d54184257116f89e80
Juan Carlos <99610897+JCdaisuki@users.noreply.github.com>
2024-11-09 19:55:00 +0100

    pag07
    Nuevo shader que es solo blanco.

--- Material.cpp
+++ Material.cpp
@@ -2,5 +2,12 @@
 
 namespace PAG
 {
+    Material::Material(glm::vec3 colorAmbiente, glm::vec3 colorDifuso, glm::vec3 colorEspecular, float brillo)
+    {
+        this->colorAmbiente = colorAmbiente;
+        this->colorDifuso = colorDifuso;
+        this->colorEspecular = colorEspecular;
+        this->brillo = brillo;
+    }
 
 }
--- Material.h
+++ Material.h
@@ -1,9 +1,20 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include <glm/vec3.hpp>
+
 namespace PAG
 {
-    class Material {
+    class Material
+    {
+        private:
+            glm::vec3 colorAmbiente;
+            glm::vec3 colorDifuso;
+            glm::vec3 colorEspecular;
+            float brillo;
+
+        public:
+            Material(glm::vec3 colorAmbiente, glm::vec3 colorDifuso, glm::vec3 colorEspecular, float brillo);
 
     };
 }
