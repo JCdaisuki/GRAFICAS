d31593afa6cce9b74829f0e26a6e6858019141f5
Juan Carlos González Martínez <99610897+JCdaisuki@users.noreply.github.com>
2024-12-02 17:03:14 +0100

    Funciona!
    La luz simplemente tenia muy poca intensidad y no se veia!

--- Renderer.cpp
+++ Renderer.cpp
@@ -38,10 +38,10 @@ namespace PAG
     {
         glEnable(GL_BLEND);
 
-        lights.push_back(new Light(Light::LightType::Ambiente, glm::vec3(0.2f), glm::vec3(1.0f), glm::vec3(0.0f), glm::vec3(0.0f)));
-        lights.push_back(new Light(Light::LightType::Puntual, glm::vec3(1.0f, 0.5f, 0.3f), glm::vec3(1.0f), glm::vec3(5.0f, 3.0f, 5.0f), glm::vec3(0.0f)));
-        lights.push_back(new Light(Light::LightType::Direccional, glm::vec3(0.8f, 0.8f, 0.8f), glm::vec3(1.0f), glm::vec3(0.0f), glm::vec3(-1.0f, -1.0f, -1.0f)));
-        lights.push_back(new Light(Light::LightType::Foco, glm::vec3(1.0f, 1.0f, 0.8f), glm::vec3(1.0f), glm::vec3(0.0f, 10.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f)));
+        lights.push_back(new Light(Light::LightType::Ambiente, glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(0.0f), glm::vec3(0.0f)));
+        lights.push_back(new Light(Light::LightType::Puntual, glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(5.0f, 3.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+        lights.push_back(new Light(Light::LightType::Direccional, glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(0.0f), glm::vec3(0.0f, 0.0f, -1.0f)));
+        lights.push_back(new Light(Light::LightType::Foco, glm::vec3(1.0f), glm::vec3(1.0f), glm::vec3(0.0f, 10.0f, 0.0f), glm::vec3(0.0f, -1.0f, 0.0f))); 
     }
 
     std::string Renderer::MostrarPropiedades()
